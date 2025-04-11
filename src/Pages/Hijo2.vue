c9485d816558e40bc9f503a53d163b98c652e92f
Daisy-GonzalezLita12 <22620056@tlaxiaco.tecnm.mx>
2025-04-11 17:39:59 -0600

    Agreagando el componente de hijo2

---
+++ src/Pages/Hijo2.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { ref } from 'vue'
+defineProps({
+  nombre: String, edad: String, carrera: String, direccion: String, telefono: String,
+})
+const count = ref(0)
+</script>
+<template>
+    <h5 style="color:coral">Soy el componente Hijo 2</h5>
+    <h5 style="color:coral">{{ nombre }}</h5>
+    <h5 style="color:coral">{{ edad }}</h5> 
+    <h5 style="color:coral">{{ carrera }}</h5>
+    <h5 style="color:coral">{{ direccion }}</h5>
+    <h5 style="color:coral">{{ telefono }}</h5>
+</template>
+
+
