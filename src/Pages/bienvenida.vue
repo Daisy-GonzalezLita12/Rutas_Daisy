1c1457a1f23504e7c683c8299c3310625d0ce74f
Daisy-GonzalezLita12 <22620056@tlaxiaco.tecnm.mx>
2025-04-11 15:26:58 -0600

    Agregando el componente bienvenida

---
+++ src/Pages/bienvenida.vue
@@ -0,0 +1,10 @@
+<template><!-- Sección de bienvenida -->
+    <!-- ese definen unos estilos -->
+        <header class=" text-white text-center py-5" style="background-color: #AD2E05;   margin-top: 0px;">
+            <!-- Título de bienvenida -->
+            <h1>Bienvenidos a Pizzería "EL RINCÓN DE LA PIZZA"</h1>
+            <!-- eslogan -->
+            <p>Las mejores pizzas,  recién hechas para ti</p>
+        </header>
+    </template>
+    
