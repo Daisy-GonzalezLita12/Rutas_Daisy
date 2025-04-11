37e69662287cfe511fd1aba73fba1a7f25368044
Daisy-GonzalezLita12 <22620056@tlaxiaco.tecnm.mx>
2025-04-11 15:51:10 -0600

    Agregando el componente pagina_pizza

---
+++ src/Pages/pagina_pizza.vue
@@ -0,0 +1,23 @@
+<script setup>
+import narvar from '../Pages/narvar.vue'; 
+import bienvenida from '../Pages/bienvenida.vue';
+import carrito from '../Pages/carrito.vue';
+import carrusel from '../Pages/carrusel.vue';
+import productos_destacados from '../Pages/productos_destacados.vue';
+import pie_de_pagina from '../Pages/pie_de_pagina.vue';
+</script>
+
+<template>
+ <div class="container">
+    <narvar/>
+  <bienvenida/>
+  <carrito />
+  <carrusel/>
+  <productos_destacados/>
+  <pie_de_pagina/>
+ </div>
+</template>
+
+<style scoped>
+
+</style>
