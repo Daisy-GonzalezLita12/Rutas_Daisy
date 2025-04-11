1976b0d2f98af383a782de0cc90eed37cb1d36a6
Daisy-GonzalezLita12 <22620056@tlaxiaco.tecnm.mx>
2025-04-11 17:43:02 -0600

    Agreagando el componente de padre1

---
+++ src/Pages/padre1.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { ref } from 'vue'
+import Hijo1 from '../Pages/hijo_1.vue'
+import Hijo2 from '../Pages/hijo_2.vue'
+const props = defineProps({
+  nombre: String,
+})
+const count = ref(0)
+</script>
+<template>
+    <div class="container">
+        <!-- Título -->
+        <h3 style="color: darkgreen;text-align: center;">Soy el componente padre</h3>
+        <!-- Se manda a traer el componente el componente "Hijo1" y se le asigna una edad-->
+         <Hijo1  class="mt-4" edad="Edad: 10 años"/>
+        <!-- Se manda a traer el componente el componente "Hijo2" y se le asigna una edad-->
+         <Hijo2  class="mt-4" edad="Edad: 4 años"/>
+    </div>
+   </template>
+
+
