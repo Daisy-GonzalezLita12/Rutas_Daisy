20ecc88ffbd1f3f44892d86b4adac18efd048db7
Daisy-GonzalezLita12 <22620056@tlaxiaco.tecnm.mx>
2025-04-11 17:35:10 -0600

    Agregando el componente de eventos

---
+++ src/Pages/Eventos1.vue
@@ -0,0 +1,68 @@
+<template>
+    <div class="container">
+      <div class="row mt-4">
+        <!-- Inicia el primer botón -->
+        <div>
+            <h4>EVENTO CLIC</h4>
+          <button @click="clic" class="btn btn-danger" type="button">Dale clic</button>         
+          <button @click="soyBoton" class="btn btn-warning mx-2" type="button">Dale clic2</button>         
+       <!-- Termina el primer botón -->
+        </div> 
+             <!-- inicia el segundo botón -->
+             <div><br>
+            <h4>EVENTO CHANGE</h4>
+            <label class="input-group-text" for="">Ingresa tu nombre: </label>
+            <input class="form-control mt-2" id="nombre" @change="nombreE">
+            <!-- Con la etiqueta párrafo -->
+             <p>El nombre ingresado es: <strong> {{ nombre }}</strong> </p><br>
+                <!-- Termina el segundo botón -->
+             </div>
+                <!-- Inicia el tercer botón-->
+                 <div>
+                  <h4>EVENTO DE MOUSEENTER Y MOUSELEAVE</h4>
+                  <div class="text-center" style="color: white; width: 300px ; height: 200px;background-color: black;text-align: center; padding: 15px;border-radius: 14px;"
+                  @mouseenter="mostrarMensaje" @mouseleave="ocultarMensaje">
+                  <p >pase el mause</p>
+                  <p >{{ mensaje }}</p>
+                 </div>        
+                 <!--Termina el tercer botón  -->
+                 </div>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+        return {
+            // Se declaran todas las variables que vayamos a ocupar.
+          nombre: '',
+          mensaje:'',
+        }
+    },
+    methods: {
+      clic() {
+        alert("Gracias por dar clic");
+      },
+      soyBoton() {
+        alert("Soy un botón");
+      },
+      nombreE(Event){
+        // Se obtiene el valor del input
+        this.nombre = Event.target.value;
+           },
+
+        //  Métodos del evento como mouseover
+        mostrarMensaje() {   
+           this.mensaje= "El mouse está dentro del rectangulo";
+            },
+            ocultarMensaje() {
+           this.mensaje= "El mouse está fuera del rectangulo";
+           },
+          }
+  }
+  </script>
+  
+  <style scoped>
+  </style>
+  
---
+++ src/Pages/Eventos2.vue
@@ -0,0 +1,66 @@
+<template>
+    <div>
+        <div class="container">
+            <div class="row">
+                 <!-- Evento doble clic -->
+                  <div>
+                    <h4>EVENTO DOBLE CLIC</h4>
+                    <button @dblclick="dobleClic" class="btn btn-primary" type="button">Doble Clic</button>
+                  </div>
+                 <!-- Evento Capturar tecla -->
+                 <div><br>
+                    <h4>EVENTO CAPTURAR TECLA</h4>
+                    <input class="from-control" @keyup="capturarTecla"  placeholder="Escribe algo">
+                    <p>última tecla presionada: <strong>{{ tecla }}</strong></p>
+                   </div>
+                    <!-- Evento ennforcar y desenfocar -->
+                          <div><br>
+                    <h4>EVENTO DE ENFOCAR Y DESENFOCAR</h4>
+                    <input class="form-control"@focus="Enfocar" @blur="Desonfocar" placeholder="Haz foco aquí">
+                    <p>{{ estado }}</p>
+                </div>
+                       <!-- Evento Mouse Down y Mouse up -->
+                    <div> <br>
+                    <h4>EVENTO MOUSEDOWN Y MOUSEUP</h4>
+                    <button @mousedown="Presionar" @mouseup="Soltar" class="btn btn-success">Presiona y suelta</button>
+                    <p>{{ estadoBoton }}</p>
+                 </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            // Se declaran todas las variables que vayamos a ocupar.
+            tecla: '',
+            estado: 'No enfocado',
+            estadoBoton: ''
+        }
+    },
+    methods: {
+        dobleClic() {
+            alert("Hiciste doble clic en el botón");
+        },
+        capturarTecla(Event) {
+            this.tecla = Event.key;
+        }, Enfocar() {
+            this.estado = "El input está enfocado";
+        },
+        Desonfocar() {
+            this.estado = "El input ha perdido el foco";
+        },
+        Presionar() {
+            this.estadoBoton = "El botón está presionado";
+        },
+        Soltar() {
+            this.estadoBoton = "El botón está suelto";
+        }
+    }
+    
+}
+</script>
+<style>
+    
+</style>
