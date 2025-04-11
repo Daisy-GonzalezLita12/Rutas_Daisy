e0bba4fe050b6694b53736e6465c56eae748f5d9
Daisy-GonzalezLita12 <22620056@tlaxiaco.tecnm.mx>
2025-04-11 14:48:38 -0600

    Agregando las imágenes, y los componentes de Home.vue y About.vue

---
+++ src/Pages/Home.vue
@@ -0,0 +1,45 @@
+<template>  
+    <h4 style="color:coral;text-align: center;">UNIDAD 3: "PROGRAMACIÓN DEL LADO DEL CLIENTE"</h4>
+    <p style="text-align:left;">Bienvenido(a)</p>
+    <p style="text-align: justify;">En este sitio se encuentran los trabajos realizados a lo largo de la unidad 3, 
+        de la asignatura de Programación Web, realizados por la alumna Daisy Gonzalez Lita, con número de control 
+         22620056, los cuales fueron fueron revisados por el ingeniero Josué Israel Vásquez Martínez. </p>
+
+    <p style="text-align: justify;">En esta unidad se trabajo con el framework de Vue, con la librería de Bootstrap, y con GitHub.</p>
+
+    <div id="carouselExampleDark" class="carousel carousel-dark slide">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="https://lenguajejs.com/vuejs/introduccion/primeros-pasos/vue-main.js-app.vue.png" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="https://theblackboxlab.com/wp-content/uploads/2021/05/v5.png" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="https://www.datocms-assets.com/15783/1601042117-github.png" class="d-block w-100" alt="...">
+    </div>
+  </div>
+
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previo</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Siguiente</span>
+  </button>
+</div>
+
+</template>
+<style scoped>
+p{
+    font-family: Arial, Helvetica, sans-serif;
+}
+</style>
+
+  
